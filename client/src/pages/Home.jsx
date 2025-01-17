82451f7e8930c190ad85679eb7ce648471652e55
Sameera lakmal <135108578+samerr22@users.noreply.github.com>
2025-01-17 10:04:15 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -44,7 +44,7 @@ export default function Home() {
 
     const fetchSaleListings = async () => {
       try {
-        const res = await fetch('/api/listing/get?type=sale&limit=4')
+        const res = await fetch('/api/listing/get?type=sale&limt=4')
         const data = await res.json();
         setSaleListings(data);
       } catch (error) {
