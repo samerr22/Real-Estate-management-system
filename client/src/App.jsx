99c8f44952f686c8c9d89e7e578ee9f3912deb24
Sameera lakmal <135108578+samerr22@users.noreply.github.com>
2025-01-19 11:00:20 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
+import Home from './pages
 import Signin from './pages/Signin'
 import SignOut from './pages/SinUp'
 import About from './pages/Ab
