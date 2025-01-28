1f5d476bd7bae9f5a2ccd5d8e9bd0e277d7f9fa9
Sameera lakmal <135108578+samerr22@users.noreply.github.com>
2025-01-28 07:38:45 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import {Swiper, SwiperSlide} from 'swiper/react'
 
-import { Navigation } from 'swiper/modules'
+
 import SwiperCore from 'swiper'
 import 'swiper/css/bundle';
 import ListingItem from "../componnts/ListingItem";
