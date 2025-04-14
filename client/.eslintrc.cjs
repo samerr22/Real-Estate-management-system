36197fc949a559cbc06797ac31ad1e045a4b60d4
Sameera lakmal <135108578+samerr22@users.noreply.github.com>
2025-04-14 20:46:13 +0530

    Update .eslintrc.cjs

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -9,10 +9,5 @@ module.exports = {
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
 
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
+  
 }
